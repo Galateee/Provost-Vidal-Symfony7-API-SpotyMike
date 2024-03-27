a1f6e93f9192bdef93a8c61a683a0e57df73d9b9
Robin Vidal <robinvidal01@gmail.com>
2024-03-27 15:05:55 +0100

    make:crud albumController

---
+++ src/Form/AlbumType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Album;
+use App\Entity\Artist;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AlbumType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('idAlbum')
+            ->add('nom')
+            ->add('categ')
+            ->add('cover')
+            ->add('year')
+            ->add('artist_User_idUser', EntityType::class, [
+                'class' => Artist::class,
+'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Album::class,
+        ]);
+    }
+}
