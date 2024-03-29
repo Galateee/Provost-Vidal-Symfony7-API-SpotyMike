b6738ebf190796a4bc983b5eddb58945919e452c
Robin Vidal <robinvidal01@gmail.com>
2024-03-29 13:48:35 +0100

    fixtures

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -5,6 +5,7 @@ namespace App\Repository;
 use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
 
 /**
  * @extends ServiceEntityRepository<User>
@@ -20,6 +21,11 @@ class UserRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, User::class);
     }
+
+    // public function upgradePassword($user, $newHashedPassword){
+    //     $user->setPassword($newHashedPassword);
+    //     $this->getEntityManager()->flush();
+    // }
 
     //    /**
     //     * @return User[] Returns an array of User objects
