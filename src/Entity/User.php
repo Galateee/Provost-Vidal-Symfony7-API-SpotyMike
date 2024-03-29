bf486e368f6ea65022b04382358b27250ff70dcb
mprovost@et.intechinfo.fr <mprovost@et.intechinfo.fr>
2024-03-29 13:52:06 +0100

    update User

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -19,22 +19,32 @@ class User
     private ?string $idUser = null;
 
     #[ORM\Column(length: 55)]
-    private ?string $name = null;
+    private ?string $firstName = null;
+
+    #[ORM\Column(length: 55)]
+    private ?string $lastName = null;
+
+    #[ORM\Column(length: 55)]
+    private ?string $sexe = null;
+
+    #[ORM\Column(length: 55)]
+    private ?\Date $dateBirth = null;
 
     #[ORM\Column(length: 80)]
     private ?string $email = null;
 
-    #[ORM\Column(length: 90)]
-    private ?string $encrypte = null;
-
     #[ORM\Column(length: 15, nullable: true)]
     private ?string $tel = null;
+
+    #[ORM\Column(length: 90)]
+    private ?string $password = null;
 
     #[ORM\Column]
     private ?\DateTimeImmutable $createAt = null;
 
     #[ORM\Column(type: Types::DATETIME_MUTABLE)]
     private ?\DateTimeInterface $updateAt = null;
+
 
     #[ORM\OneToOne(mappedBy: 'User_idUser', cascade: ['persist', 'remove'])]
     private ?Artist $artist = null;
@@ -56,14 +66,50 @@ class User
         return $this;
     }
 
-    public function getName(): ?string
+    public function getfirstName(): ?string
     {
-        return $this->name;
+        return $this->firstName;
     }
 
-    public function setName(string $name): static
+    public function setfirstName(string $firstName): static
     {
-        $this->name = $name;
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getlastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    public function setlastName(string $lastName): static
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    public function getsexe(): ?string
+    {
+        return $this->sexe;
+    }
+
+    public function setsexe(string $sexe): static
+    {
+        $this->sexe = $sexe;
+
+        return $this;
+    }
+
+    public function getdateBirth(): ?\Date
+    {
+        return $this->dateBirth;
+    }
+
+    public function setdateBirth(\Date $dateBirth): static
+    {
+        $this->dateBirth = $dateBirth;
 
         return $this;
     }
@@ -80,14 +126,14 @@ class User
         return $this;
     }
 
-    public function getEncrypte(): ?string
+    public function getPassword(): ?string
     {
-        return $this->encrypte;
+        return $this->password;
     }
 
-    public function setEncrypte(string $encrypte): static
+    public function setPassword(string $password): static
     {
-        $this->encrypte = $encrypte;
+        $this->password = $password;
 
         return $this;
     }
