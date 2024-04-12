dfdd3534858edcbe320ec2b0f2428c9396a0a157
mprovost@et.intechinfo.fr <mprovost@et.intechinfo.fr>
2024-04-12 09:38:59 +0200

    add jwt creation token

--- src/Controller/TokenVerifierService.php
+++ src/Controller/TokenVerifierService.php
@@ -46,6 +46,11 @@ class TokenVerifierService {
         return false;
     }
 
+    public function createToken(string $email): string
+    {
+        return $this->jwtProvider->create(["email" => $email, "exp" => time()+120, "iat" => time()])->getToken();
+    }
+
     public function sendJsonErrorToken($nullToken): Array
     {
         return [
